a75f831ccf24e00ab1639d3f08666026cbfeec0d
Elkin <elkin148@hotmail.com>
2024-07-09 23:48:23 -0500

    prueba 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { Route, Routes } from "react-router";
 import First from "./components/First";
 import Home from "./components/Home";
+import { HashRouter as Router } from "react-router-dom";
 import Second from "./components/Second";
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/first" element={<First />} />
-        <Route path="/second" element={<Second />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/first" element={<First />} />
+          <Route path="/second" element={<Second />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
